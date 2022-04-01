618b4a41087467c302a487f617a97c07678d23fc
Divanshu Chauhan <23524935+Divkix@users.noreply.github.com>
2022-04-01 16:16:05 -0700

    bump to v1.1.0  - Add AioHttp helper to url_helpers

--- README.md
+++ README.md
@@ -53,9 +53,22 @@ print(ns.c.e)
 ### pypers.url_helpers:
 ```python
 from pypers.url_helpers import UrlHelpers
+from pypers.url_helpers import AioHttp
 
 new_url = await UrlHelpers.shorten_url('https://www.google.com')
 print(new_url)
+
+json, resp = AioHttp.get_json('https://www.google.com')
+print(json, resp)
+
+text, resp = AioHttp.get_text('https://www.google.com')
+print(text, resp)
+
+raw, resp = AioHttp.get_raw('https://www.google.com')
+print(raw, resp)
+
+json, resp = AioHttp.post_json('https://www.google.com')
+print(json, resp)
 ```
 
 ### pypers.image_tools:
--- pypers/url_helpers.py
+++ pypers/url_helpers.py
@@ -1,4 +1,7 @@
 from httpx import AsyncClient
+from typing import Any, Dict, Tuple
+
+from aiohttp import ClientResponse, ClientSession
 
 
 class UrlHelpers:
@@ -23,3 +26,86 @@ class UrlHelpers:
         async with AsyncClient() as client:
             r = await client.get(api_url)
         return r.text
+
+
+class AioHttp:
+    """
+    Class to help with the creation and handling of aiohttp requests.
+    """
+
+    @staticmethod
+    async def get_json(
+        link: str,
+        **kwargs,
+    ) -> Tuple[Dict[str, Any], ClientResponse]:
+        """
+        Get the JSON response from a link.
+
+        Args:
+            link: The URL to send request to.
+
+        Returns:
+            The JSON response from the link.
+            The response object.
+        """
+        async with ClientSession() as session:
+            async with session.get(link, **kwargs) as resp:
+                return await resp.json(), resp
+
+    @staticmethod
+    async def get_text(
+        link: str,
+        **kwargs,
+    ) -> Tuple[str, ClientResponse]:
+        """
+        Get the text response from a link.
+
+        Args:
+            link: The URL to send request to.
+
+        Returns:
+            The TEXT response from the link.
+            The response object.
+        """
+        async with ClientSession() as session:
+            async with session.get(link, **kwargs) as resp:
+                return await resp.text(), resp
+
+    @staticmethod
+    async def get_raw(
+        link: str,
+        **kwargs,
+    ) -> Tuple[bytes, ClientResponse]:
+        """
+        Get the raw response from a link.
+
+        Args:
+            link: The URL to send request to.
+
+        Returns:
+            The RAW response from the link.
+            The response object.
+        """
+        async with ClientSession() as session:
+            async with session.get(link, **kwargs) as resp:
+                return await resp.read(), resp
+
+    @staticmethod
+    async def post_json(
+        link: str,
+        data: Dict[str, Any],
+        **kwargs,
+    ) -> Tuple[Dict[str, Any], ClientResponse]:
+        """
+        Post the JSON data to a link.
+
+        Args:
+            link: The URL to post request to.
+
+        Returns:
+            The JSON response from the link.
+            The response object.
+        """
+        async with ClientSession() as session:
+            async with session.post(link, json=data, **kwargs) as resp:
+                return await resp.json(), resp
