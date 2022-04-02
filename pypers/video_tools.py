0f75ec1195726969e164bf02e220d17adf5db0bb
Divanshu Chauhan <23524935+Divkix@users.noreply.github.com>
2022-04-02 01:48:45 -0700

    Make extract_video_tools staticmethod

--- pypers/video_tools.py
+++ pypers/video_tools.py
@@ -3,6 +3,7 @@ from hachoir.parser import createParser
 
 
 class VideoTools:
+    @staticmethod
     def extract_vid_data(output_vid: str):
         metadata = extractMetadata(createParser(output_vid))
         duration = metadata.get("duration").seconds if metadata.has("duration") else 0
